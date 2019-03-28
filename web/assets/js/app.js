d825da509fe720f9e994b1f0af8d7fc7332ce0a7
Noldor5150 <ruikis.paulius@gmail.com>
2019-03-28 19:44:52 +0200

    right sidebar menu su linija, padarem

--- web/assets/js/app.js
+++ web/assets/js/app.js
@@ -6,6 +6,10 @@ $(function(){
     $('#side-menu').toggleClass('side-menu--closed');
     $('body').toggleClass('sidenav-closed');
   })
+
+   $('#notifications').on('click', function() {
+   $('#side-menu-right').toggleClass('side-menu--right--closed');
+})
   var basename = $('#side-menu').find('li.active span').text();
   $('#basename').text(basename)
   .attr('href', '/' + basename.toLowerCase() + '.html');
